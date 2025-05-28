a32d03031ab0a88bf3f55485b2e7f613787e2420
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2025-05-28 10:18:55 -0500

    avance

--- src/config/global.js
+++ src/config/global.js
@@ -1,9 +1,10 @@
 export default {
   global: {
-    Name: 'Nombre del recurso educativo',
-    Description: 'Descripción del RED',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
-    fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
+    Name: 'Creando experiencias desde la reserva',
+    Description:
+      'Para incursionar en el aprendizaje sobre el proceso de reservas, es fundamental comprender los conceptos básicos que hacen posible la magia de la hospitalidad. Aprende sobre los tipos de alojamiento, los servicios que se prestan dentro del mismo, las funciones que desempeña el área de reservas para que cada tipo de clientes que requieren del servicio se sientan como en su hogar lejos de su casa.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.png'),
+    fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.jpg'),
     imagenesDecorativasBanner: [
       {
         clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
@@ -31,28 +32,160 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Normativa de formalización',
         desarrolloContenidos: true,
-        subMenu: [
-          {
-            numero: '1.1',
-            titulo: 'Subtema 1',
-            hash: 't_1_1',
-          },
-        ],
       },
 
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo:
+          'Apropiación de conocimientos básicos dentro de un establecimiento de alojamiento',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Tipos de alojamiento y hospedajes',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Categorización de los hoteles',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Tipos de habitaciones y acomodación',
+            hash: 't_2_3',
+          },
+          {
+            numero: '2.4',
+            titulo: 'Portafolio de servicios ',
+            hash: 't_2_4',
+          },
+          {
+            numero: '2.5',
+            titulo: 'Clientes – tipos de clientes – características',
+            hash: 't_2_5',
+          },
+          {
+            numero: '2.6',
+            titulo: 'Área de reservas ',
+            hash: 't_2_6',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Tema 3',
+        titulo: 'Comunicación',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: 'Tipos de comunicación',
+            hash: 't_3_1',
+          },
+          {
+            numero: '3.2',
+            titulo: 'Canales de comunicación',
+            hash: 't_3_2',
+          },
+          {
+            numero: '3.3',
+            titulo: 'Técnicas de comunicación  ',
+            hash: 't_3_3',
+          },
+          {
+            numero: '3.4',
+            titulo: 'Clasificación según la trasmisión',
+            hash: 't_3_4',
+          },
+          {
+            numero: '3.5',
+            titulo: 'Habilidades comunicativas',
+            hash: 't_3_5',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema4',
+        numero: '4',
+        titulo: 'Servicio al cliente',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '4.1',
+            titulo: 'Tipos de servicio al cliente',
+            hash: 't_4_1',
+          },
+          {
+            numero: '4.2',
+            titulo: 'Protocolos en el servicio al cliente',
+            hash: 't_4_2',
+          },
+          {
+            numero: '4.3',
+            titulo: '4.3	Ética del servicio',
+            hash: 't_4_3',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema5',
+        numero: '5',
+        titulo: 'Sistema de información de alojamiento turístico',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '5.1',
+            titulo: 'Técnicas de recolección de datos',
+            hash: 't_5_1',
+          },
+          {
+            numero: '5.2',
+            titulo: 'Técnicas de registro de datos',
+            hash: 't_5_2',
+          },
+          {
+            numero: '5.3',
+            titulo: 'Ley 1581 de 2012 – Ley de protección de datos personales',
+            hash: 't_5_3',
+          },
+          {
+            numero: '5.4',
+            titulo: 'Documentos de identidad legales',
+            hash: 't_5_4',
+          },
+          {
+            numero: '5.5',
+            titulo: 'Técnicas de validación de datos',
+            hash: 't_5_5',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema6',
+        numero: '6',
+        titulo: 'Estrategias de venta',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '6.1',
+            titulo: 'Técnicas',
+            hash: 't_6_1',
+          },
+          {
+            numero: '6.2',
+            titulo: 'Políticas',
+            hash: 't_6_2',
+          },
+          {
+            numero: '6.3',
+            titulo: 'Manejo de objeciones',
+            hash: 't_6_3',
+          },
+        ],
       },
     ],
     subMenu: [
--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,15 +2,23 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. 
-    
-
+    p La hotelería creada desde un principio para brindar hospitalidad a los viajeros que necesitan pernoctar y tener sus servicios básicos como alojamiento. 
+    p Desde ahí se crea la necesidad de brindar atención, bienestar y confort a un turista que se traslada de un lugar a otro y busca un lugar seguro para descansar y atender sus necesidades primarias (higiene, descanso, alimentación). 
+    p Con el paso del tiempo la legislación colombiana estableció leyes que rigen esta actividad de manera responsable, organizada y articulada con los demás sectores turísticos como agencias de viajes, cadenas de transporte y guías profesionales entre otros. 
+    p En el siguiente mapa conceptual se puede identificar como primera línea la normativa de formalización con las leyes que regulan esta actividad, los conceptos básicos de los establecimientos de alojamiento y hospedaje, los tipos de alojamiento y hospedaje que están definidos en la norma, la clasificación que se les da en Colombia según su categoría; y los tipos de habitación que de acuerdo con la norma, puede establecer un establecimiento de alojamiento y hospedaje.
+    p Como segunda línea del mapa conceptual se encuentran los servicios complementarios que ofrece el establecimiento de alojamiento según su categoría, los tipos de clientes que llegan a hospedarse, el área de reservas, cuya actividad principal es dar atención a todos los clientes que necesitan solicitar un servicio de hospedaje con el establecimiento y dar solución a las peticiones del cliente.
+    p Como tercera línea del mapa conceptual se aprecia el tema de la comunicación como base fundamental para enlazar una conversación con el cliente clara, oportuna, respetuosa y cordial.
+    p Luego se encuentra la línea de servicio al cliente donde se aprecia la importancia de brindar satisfacción al cliente utilizando técnicas, protocolos en el servicio al cliente y aplicando la ética en el servicio.
+    p Encontramos en la quinta línea el sistema de información de alojamiento turístico que tiene como finalidad el registro de los datos del cliente.
+    p Y por último en la sexta línea no menos importante que las demás se encuentran las estrategias que se pueden utilizar para ofrecer un producto y lograr una venta efectiva.  
+ 
+  
     .row.justify-content-center
-      .col-lg-10.mb-5.bgs.p-4.brad
+      .col-lg-12.mb-5.bgs.p-4.brad
         figure
           img(src="@/assets/curso/sintesis.svg", alt="Descripción imagen")
       .col-auto
-        a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
+        a.anexo.mb-5(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
             img(src="@/assets/bullets/icono-pdf.svg")
           .anexo__texto
