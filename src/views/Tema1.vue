958236b5a66b7c3578af91bd03967bcfb5057eb1
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2025-05-28 15:06:04 -0500

    Avance

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -7,90 +7,91 @@
           span 1
         h1 Normativa de formalización
       
-      p.mb-5 La Ley 300 de 1996 expide la ley general de turismo, la cual se modificó por la Ley 1558 de 2012 y por la Ley 1101 de 2006.  
+      p.mb-4 La Ley 300 de 1996 expide la ley general de turismo, la cual se modificó por la Ley 1558 de 2012 y por la Ley 1101 de 2006.  
 
+      .fondo1
+        .py-3
+        .row.justify-content-center.align-items-center.mb-5
+          .col-lg-10
+            LineaTiempoD.color-acento-contenido
+              .row.justify-content-center.align-items-center(numero="2012" titulo="La Ley 1558 de 2012")
+                .col-lg-3.col-8.col-md-4
+                  figure
+                    img(src='@/assets/curso/Tema1/img01.png', alt='')
+                .col-lg-9.mb-4.mb-md-0
+                  p #[b En su artículo 2], establece que el turismo es una industria esencial para el desarrollo del país y en especial de las diferentes entidades territoriales, regionales, provincias y que cumple una función social.
+                  p El estado le dará especial protección en razón de su importancia para el desarrollo nacional.
+              
+              .row.justify-content-center(numero="1996" titulo="Capítulo I de la Ley 300 de 1996")
+                .col-lg-3.col-8.col-md-4
+                  figure
+                    img(src='@/assets/curso/Tema1/img02.png', alt='')
+                .col-lg-9.mb-4.mb-md-0
+                  p #[b Llamado “De los prestadores de servicios turísticos en particular”,] el artículo 76 hace referencia a toda persona natural o jurídica que habitualmente proporcione, intermedie o contrate directa o indirectamente con el turista, la prestación de los servicios a que se refiere esta ley y que se encuentre inscrito en el Registro Nacional de Turismo.
+              
+              .row.justify-content-center(numero="1996" titulo="Artículo 62 de la Ley 300 de 1996")
+                .col-lg-3.col-8.col-md-4
+                  figure
+                    img(src='@/assets/curso/Tema1/img03.png', alt='')
+                .col-lg-9.mb-4.mb-md-0
+                  p #[b “Prestadores de servicios turísticos”,] (reglamentado por el Decreto Nacional 504 de 1997 y modificado por el art. 12. Ley 1101 de 2006), son prestadores de servicios turísticos:        
+                  ul.lista-ul--color
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Los hoteles, centros vacacionales, campamentos, viviendas turísticas y otros tipos de hospedaje no permanente, excluidos los establecimientos que prestan el servicio de alojamiento por horas.
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Las agencias de viajes y turismo, agencias mayoristas y las agencias operadoras.
 
-      .row.justify-content-center.align-items-center.mb-5
-        .col-lg-10
-          LineaTiempoD.color-acento-contenido
-            .row.justify-content-center.align-items-center(numero="2012" titulo="La Ley 1558 de 2012")
-              .col-lg-3.col-8.col-md-4
-                figure
-                  img(src='@/assets/curso/Tema1/img01.png', alt='')
-              .col-lg-9.mb-4.mb-md-0
-                p #[b En su artículo 2], establece que el turismo es una industria esencial para el desarrollo del país y en especial de las diferentes entidades territoriales, regionales, provincias y que cumple una función social.
-                p El estado le dará especial protección en razón de su importancia para el desarrollo nacional.
-            
-            .row.justify-content-center(numero="1996" titulo="Capítulo I de la Ley 300 de 1996")
-              .col-lg-3.col-8.col-md-4
-                figure
-                  img(src='@/assets/curso/Tema1/img02.png', alt='')
-              .col-lg-9.mb-4.mb-md-0
-                p #[b Llamado “De los prestadores de servicios turísticos en particular”,] el artículo 76 hace referencia a toda persona natural o jurídica que habitualmente proporcione, intermedie o contrate directa o indirectamente con el turista, la prestación de los servicios a que se refiere esta ley y que se encuentre inscrito en el Registro Nacional de Turismo.
-            
-            .row.justify-content-center(numero="1996" titulo="Artículo 62 de la Ley 300 de 1996")
-              .col-lg-3.col-8.col-md-4
-                figure
-                  img(src='@/assets/curso/Tema1/img03.png', alt='')
-              .col-lg-9.mb-4.mb-md-0
-                p #[b “Prestadores de servicios turísticos”,] (reglamentado por el Decreto Nacional 504 de 1997 y modificado por el art. 12. Ley 1101 de 2006), son prestadores de servicios turísticos:        
-                ul.lista-ul--color
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Los hoteles, centros vacacionales, campamentos, viviendas turísticas y otros tipos de hospedaje no permanente, excluidos los establecimientos que prestan el servicio de alojamiento por horas.
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Las agencias de viajes y turismo, agencias mayoristas y las agencias operadoras.
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Las oficinas de representaciones turísticas.
 
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Las oficinas de representaciones turísticas.
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Los guías de turismo.
 
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Los guías de turismo.
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Los operadores profesionales de congresos, ferias y convenciones.
 
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Los operadores profesionales de congresos, ferias y convenciones.
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Los arrendadores de vehículos para turismo nacional e internacional.
 
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Los arrendadores de vehículos para turismo nacional e internacional.
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Los usuarios operadores, desarrolladores e industriales en zonas francas turísticas.
 
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Los usuarios operadores, desarrolladores e industriales en zonas francas turísticas.
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Las empresas promotoras y comercializadoras de proyectos de tiempo compartido y multipropiedad.
 
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Las empresas promotoras y comercializadoras de proyectos de tiempo compartido y multipropiedad.
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Los establecimientos de gastronomía y bares turísticos.
 
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Los establecimientos de gastronomía y bares turísticos.
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Las empresas captadoras de ahorro para viajes y de servicios turísticos prepagados.
 
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Las empresas captadoras de ahorro para viajes y de servicios turísticos prepagados.
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Los concesionarios de servicios turísticos en parque.
 
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Los concesionarios de servicios turísticos en parque.
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Las empresas de transporte terrestre automotor especializado, las empresas operadoras de chivas y de otros vehículos automotores que presten servicio de transporte turístico.
 
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Las empresas de transporte terrestre automotor especializado, las empresas operadoras de chivas y de otros vehículos automotores que presten servicio de transporte turístico.
-
-                  li.d-flex
-                    i.fas.fa-circle-check
-                    p.mb-0 Los demás que el Ministerio de Comercio, Industria y Turismo determine. 
-            .row.justify-content-center(numero="1996" titulo="Capitulo II artículo 78")
-              .col-lg-3.col-8.col-md-4
-                figure
-                  img(src='@/assets/curso/Tema1/img04.png', alt='')
-              .col-lg-9.mb-4.mb-md-0
-                p #[b “De los establecimientos hoteleros o de hospedaje”] Se entiende por establecimiento hotelero o de hospedaje, el conjunto de bienes destinados por la persona natural o jurídica a prestar el servicio de alojamiento no permanente inferior a 30 días, con o sin alimentación y servicios básicos y/o complementarios o accesorios de alojamiento, mediante contrato de hospedaje. 
+                    li.d-flex
+                      i.fas.fa-circle-check
+                      p.mb-0 Los demás que el Ministerio de Comercio, Industria y Turismo determine. 
+              .row.justify-content-center(numero="1996" titulo="Capitulo II artículo 78")
+                .col-lg-3.col-8.col-md-4
+                  figure
+                    img(src='@/assets/curso/Tema1/img04.png', alt='')
+                .col-lg-9.mb-4.mb-md-0
+                  p #[b “De los establecimientos hoteleros o de hospedaje”] Se entiende por establecimiento hotelero o de hospedaje, el conjunto de bienes destinados por la persona natural o jurídica a prestar el servicio de alojamiento no permanente inferior a 30 días, con o sin alimentación y servicios básicos y/o complementarios o accesorios de alojamiento, mediante contrato de hospedaje. 
 
       .tarjeta.color-acento-contenido.p-4.mb-0(data-aos="fade-right")
         .row.justify-content-around.align-items-center
---
+++ src/views/Tema6.vue
@@ -0,0 +1,212 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 6
+      h1 Estrategias de venta
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade-right")
+      .col-lg-9
+        p En el sector hotelero las ventas no son exclusividad de unos pocos funcionarios, es responsabilidad de todos los empleados de la organización, cada uno ejerce influencia sobre los clientes, por lo tanto, la disposición a prestar un excelente servicio debe ser una estrategia permanente.  
+        .row.justify-content-center.align-items-center
+          .col-lg-12
+            .bg12.mb-5
+              .row.justify-content-center.align-items-center.mb-5
+                .col-lg-6.d-none.d-lg-block(data-aos="fade-right")
+                  .bg11.p-3
+                    img.img-a.img-t(src='@/assets/curso/Tema6/img02.png', alt='')
+                .col-lg-6.p-4(data-aos="fade-left")
+                  .bg3.p-1.mb-4
+                    h4.mb-0(data-aos="fade-left") Definición
+                  p.mb-0(data-aos="fade-right") Venta es una acción que se genera de vender un bien o servicio que se encuentra a disposición del público a cambio de dinero. 
+      .col-lg-3.col-8.col-md-5
+        img.mb-3(src='@/assets/curso/Tema6/img01.png', alt='')
+
+      p.mb-4 En el sector hotelero se deben definir una serie de variantes para ofrecer el servicio como son: 
+
+      .row.justify-content-center.align-items-center.mb-2(data-aos="fade-right")
+        .col-lg-5
+          img.mb-3(src='@/assets/curso/Tema6/img03.png', alt='')
+        .col-lg-7
+          ul.lista-ul--color
+            li.d-flex.mb-0
+              i.fas.fa-circle-check
+              p.mb-0 ¿Qué busca el cliente que llama?
+            li.d-flex.mb-0
+              i.fas.fa-circle-check
+              p.mb-0 Disponibilidad de habitaciones.
+            li.d-flex.mb-0
+              i.fas.fa-circle-check
+              p.mb-0 Movimiento de entrada y salida de huéspedes.
+            li.d-flex.mb-0
+              i.fas.fa-circle-check
+              p.mb-0 Número de días a necesitar el servicio.
+            li.d-flex.mb-0
+              i.fas.fa-circle-check
+              p.mb-0 Precios.
+            li.d-flex.mb-0
+              i.fas.fa-circle-check
+              p.mb-0 Requerimiento de servicios complementarios como alimentación, bebidas, salón de eventos, sala de negocios, entre otros.
+      separador
+      #t_6_1.titulo-segundo.mb-5
+        h2 6.1 Técnicas
+
+      p La comunicación es el elemento fundamental dentro de las técnicas de ventas se da presencial y no presencial (telefónicamente o escrita): 
+      p.mb-5 En la venta se repiten las fases de la negociación desde la planificación del producto o servicio a ofrecer, acercamiento o presentación del producto o servicio, argumentación del ofrecimiento y/o manejo de las objeciones y cierre. Dentro de algunas técnicas podemos mencionar:
+
+      .row.justify-content-center.mb-5(data-aos="fade-right")
+        .col-lg-3.col-8.col-md-5
+          img.mb-3(src='@/assets/curso/Tema6/img04.png', alt='')
+        .col-lg-9 
+          AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+            .row.justify-content-center(titulo="Modelo AIDA")
+              .col-md-11.mb-4.mb-md-0
+                p Son las iniciales de: 
+                p.mb-0 #[b Atención:] se lleva al cliente a fijarse en lo que se le quiere vender.
+                p.mb-0 #[b Interés:] animar al cliente a interesarse estimulándolo con los beneficios.
+                p.mb-0 #[b Deseo:] lograr que el cliente quiera adquirir el producto.
+                p #[b Acción:] se efectúa la venta. 
+
+            .row.justify-content-center(titulo="Técnica SPIN")
+              .col-md-11.mb-4.mb-md-0
+                p Consiste en obtener el máximo de información por parte del cliente con base en hacer una cantidad de preguntas, con el fin de ofrecerle un mejor servicio.  
+                p.mb-0 #[b Situación:] preguntas que ayudan a identificar qué motiva al cliente a comprar.
+                p.mb-0 #[b Problema:] preguntas para detectar las dificultades o indecisiones del cliente para ayudarle a que se sienta comprendido.
+                p.mb-0 #[b Implicación:]  preguntar para que el cliente vea las consecuencias negativas si no toma rápidamente una decisión.
+                p #[b Necesidad o beneficio:]  preguntas para que el cliente quede convencido que sus necesidades serán satisfechas.
+
+            .row.justify-content-center(titulo="Sistema de entrenamiento Zelev Noel ")
+              .col-md-11.mb-4.mb-md-0
+                p Propuesta sobre el acróstico VENTAS 
+                p.mb-0 Verificación de preparativos: preparar todos los elementos a usar para el momento del acercamiento con el cliente.
+                p.mb-0 Entrevista efectiva y vendedora para detectar la tipología de cliente.
+                p.mb-0 Necesidades establecidas previamente planteando preguntas para buscar las necesidades del cliente.
+                p.mb-0 Tarea de demostración del producto mostrando las características y señalando los beneficios.
+                p.mb-0 Acción de negociación buscando un pacto rápido y seguro.
+                p Satisfacción total y posventa consiguiendo la satisfacción del cliente y buen resultado. 
+
+      separador
+      #t_6_2.titulo-segundo.mb-5
+        h2 6.2 Políticas 
+      .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5.mb-5(data-aos="fade-down")
+        .bloque-texto-g__img(
+          :style="{'background-image':`url(${require('@/assets/curso/Tema6/img05.png')})`}")
+        .bloque-texto-g__texto.p-4.py-lg-5
+          br
+          p.mb-5 La empresa establece una serie de normas y reglas que rigen las actividades diarias del establecimiento a lo que se le llaman políticas.
+          br
+          br
+          p El departamento de ventas es el área encargada, con aprobación de la alta gerencia de establecer las políticas de ventas y negociación ya que esta área conoce el producto, su costo y considera la oferta y la demanda. 
+          br
+          br
+      separador
+      #t_6_3.titulo-segundo.mb-5
+        h2 6.3 Manejo de objeciones
+      
+      .row.justify-content-center.align-items-center(data-aos="fade-right")
+        .col-lg-12
+          .bg13.mb-4
+            .row.justify-content-center.align-items-center.mb-0
+              .col-lg-1.d-none.d-lg-block(data-aos="fade-right")
+                .bg11.p-3
+                  img.img-a.img-t(src='@/assets/curso/Tema6/img06.png', alt='')
+              .col-lg-11.col-12(data-aos="fade-left")
+                .bg13.p-1.mb-3
+                  .py-2
+                  p.mb-0(data-aos="fade-right") Las objeciones son los obstáculos que se presentan en un momento determinado, se utiliza el “no” o el “pero” por parte del cliente, esto lo puede hacer para asegurarse que el producto o servicio cumplirá sus expectativas.
+      
+      p Es necesario que el vendedor desarrolle la habilidad para dar un manejo acertado y se efectúe la venta, por esta razón necesitará conocer muy bien los servicios que se ofrecen en el establecimiento, y tener la capacidad de transmitir la información y dar la seguridad al cliente que obtendrá lo que está buscando
+
+      .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+        .col-sm-12.col-lg-12
+          .d-flex.align-items-center.mt-3
+            img.w-card-icon(src="@/assets/curso/Tema6/casa.png")
+            h3.bg7.py-2.w-card-text Cierre de la venta
+
+      .row.justify-content-center.align-items-center.mb-0
+        .col-lg-8
+          .row.justify-content-center.align-items-center
+            .col-lg-12
+              .bg23.mb-0
+                .row.justify-content-center.align-items-center.mb-0
+                  .col-lg-7(data-aos="fade-left")
+                    .bg23.p-2.mb-3.px-3
+                      .py-2
+                      p.mb-0(data-aos="fade-right") Mediante el proceso de venta se utilizan argumentos y material de apoyo, que permiten captar la atención e interés del usuario, no obstante, durante el desarrollo se pueden presentar errores de apreciación, lo cual obstaculiza la venta, sin embargo, se entiende que la tarea principal del vendedor es lograr que el usuario realice la compra o utilice el servicio que se le está ofreciendo.
+                  .col-lg-5.d-none.d-lg-block(data-aos="fade-right")
+                    .bg8.p-3
+                      img.img-a.img-t(src='@/assets/curso/Tema6/img08.png', alt='')
+                  
+
+        .col-lg-4.mt-5
+          .row.align-items-center.mb-0
+            .col-lg-2.col-3.col-md-2
+              img.mb-3(src='@/assets/curso/Tema6/img07.png', alt='')
+            .col-lg-12
+              p Posterior al proceso mencionado se realiza el cierre de la venta en donde su objetivo principal es precisamente que el cliente realice la solicitud formal del producto o servicio. En esta parte del proceso el vendedor siempre debe actuar con actitud positiva demostrando que se ha llegado a un acuerdo, y concluirá mencionando los beneficios que el cliente está aceptando.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema6',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
