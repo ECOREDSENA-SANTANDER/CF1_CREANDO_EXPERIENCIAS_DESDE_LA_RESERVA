bb407507b0f997849bbafdf3c055e2335c1bbf90
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2025-06-03 16:48:58 -0500

    Avance

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -27,7 +27,7 @@
           h3.bg7.py-2.w-card-text Para tener en cuenta
 
     .row.justify-content-center.align-items-center.mb-5
-      .col-lg-5.mb-3.col-10
+      .col-lg-5.mb-3.col-10.col-md-7
         img(src='@/assets/curso/Tema4/img03.png', alt='')
       .col-lg-7 
         p La atención al cliente se refiere al trato que le damos al cliente cuando interactuamos con él, se trata de una relación con cortesía, rapidez, confiabilidad, información, simpatía, empatía y sensibilidad. 
@@ -50,23 +50,217 @@
     
     p Para brindar una buena atención al cliente es fundamental adaptarse al canal o medio a través del cual se brinda este servicio.  
     p Se encuentran:
+    .row.fondotema4.mb-4
+      .col.lg-12
+        .row.justify-content-center.align-items-center.mb-5
+          .col-lg-9.mb-4
+            TabsA.color-acento-contenido(data-aos="fade-down")
+              .tarjeta.color-acento-botones--borde.p-4(titulo="Atención presencial")
+                p Se trata de una relación cara a cara entre empresa y cliente.
+              
+              .tarjeta.color-acento-botones--borde.p-4(titulo="Atención telefónica")
+                p Este tipo de atención exige una serie de normas de etiqueta para asegurar la atención al cliente. Es importante cuidar el tono de voz, la fluidez al hablar, claridad en la información, disponibilidad y respuestas ágiles.
+              .tarjeta.color-acento-botones--borde.p-4(titulo="Atención virtual")
+                p Se incluyen recursos tecnológicos en el servicio de atención al cliente, podemos encontrar canales como: correo electrónico, chat en vivo, redes sociales. 
+              .tarjeta.color-acento-botones--borde.p-4(titulo="Atención proactiva")
+                p Es un servicio que una compañía brinda cuando se pone en contacto con los consumidores sin que ellos lo esperen. Se utiliza cuando una empresa desea presentar un nuevo producto o servicio o presentar una oferta o beneficio.  
+              .tarjeta.color-acento-botones--borde.p-4(titulo="Atención reactiva")
+                p Es cuando el cliente se coloca en contacto con la empresa en cualquier motivo; y la empresa debe estar lista a responder a cualquier solicitud por parte del cliente.   
+          
+          .col-lg-3.col-md-5.col-6
+            img(src='@/assets/curso/Tema4/img04.png', alt='')
+
+    .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+      .col-sm-12.col-lg-12
+        .d-flex.align-items-center
+          img.w-card-icon(src="@/assets/curso/Tema6/casa.png")
+          h3.bg7.py-2.w-card-text Para tener en cuenta
+
+    .row.justify-content-center.align-items-center.mb-5.fondotema42
+      .col-lg-12
+        img(src='@/assets/curso/Tema4/img05.svg', alt='')
 
 
+    separador
+    #t_4_2.titulo-segundo.mb-5
+      h2 4.2 Protocolos en el servicio al cliente  
+
+    p.mb-3 Un protocolo de servicio al cliente es un paso a paso definido por el establecimiento donde se indican las pautas o maneras de brindar un servicio al cliente según el canal por donde el cliente se comunique con el establecimiento. 
+    h5.mb-5 Dentro de un protocolo de servicio al cliente en el área de reservas se siguen los siguientes pasos:
+
+    .row.justify-content-center.mb-5
+      .col-lg-6.mb-4
+        LineaTiempoD.color-acento-contenido
+          .row.justify-content-center.align-items-center(numero="1" titulo="Saludar")
+            .col-lg-12
+              p Saludar y dar la bienvenida acompañada con una sonrisa y con un saludo formal de buenos días/buenas tardes/buenas noches/bienvenido al establecimiento (se nombra el establecimiento), y se le indica al cliente el nombre de la persona quien lo está atendiendo. 
+          .row.justify-content-center.align-items-center(numero="2" titulo="Ofrecer ayuda")
+            .col-lg-12
+              p Se ofrece ayuda al cliente, preguntando: ¿En qué puedo asistirle?.
+          .row.justify-content-center.align-items-center(numero="3" titulo="Escucha activa")
+            .col-lg-12
+              p Tener una escucha activa y una comunicación clara con el cliente.
+          .row.justify-content-center.align-items-center(numero="4" titulo="Lenguaje claro")
+            .col-lg-12
+              p Utilizar un lenguaje claro, sencillo y profesional.
+          .row.justify-content-center.align-items-center(numero="5" titulo="Solicitar información")
+            .col-lg-12
+              p Solicitar información necesaria de manera organizada: fechas, tipo de habitación, número de huéspedes.
+          .row.justify-content-center.align-items-center(numero="6" titulo="Verificar y ofrecer")
+            .col-lg-12
+              p Verificar disponibilidad y ofrecer alternativas.
+          .row.justify-content-center.align-items-center(numero="7" titulo="Tarifas y políticas")
+            .col-lg-12
+              p Explicar tarifas y políticas de cancelación.    
+          
+      .col-lg-6.col-md-8  
+        img(src='@/assets/curso/Tema4/img06.png', alt='')
+    
+    .row.justify-content-center.mb-5
+      .col-lg-6.col-md-8.mb-4
+        img(src='@/assets/curso/Tema4/img07.png', alt='')
+      .col-lg-6
+        LineaTiempoD.color-acento-contenido
+          .row.justify-content-center.align-items-center(numero="8" titulo="Reservar y confirmar")
+            .col-lg-12
+              p Confirmar la reserva y proporcionar un número de confirmación. 
+          .row.justify-content-center.align-items-center(numero="9" titulo="Detalles de reserva")
+            .col-lg-12
+              p Enviar un correo electrónico o mensaje de texto con los detalles de la reserva.           
+          .row.justify-content-center.align-items-center(numero="10" titulo="Ofrecer soluciones")
+            .col-lg-12
+              p Ofrecer soluciones rápidas y efectivas a las quejas, peticiones y solicitudes de los clientes. 
+          .row.justify-content-center.align-items-center(numero="11" titulo="Etiqueta y cortesía")
+            .col-lg-12
+              p Utilizar normas de etiqueta y cortesía, manteniendo una actitud positiva en todo momento y profesional, utilizar “por favor” y “gracias” con frecuencia, tratar a todos los clientes con respeto, evitar comentarios o juicios personales, mantener la confidencialidad de la información del cliente. 
+          .row.justify-content-center.align-items-center(numero="12" titulo="Postura profesional")
+            .col-lg-12
+              p Mantener una postura erguida y profesional, hacer contacto visual con el cliente, evitar cruzar los brazos o piernas, mantener el área de trabajo limpia y organizada.  
+          
+    .tarjeta.color-acento-contenido.p-4.mb-4(data-aos="fade-right")
+        .row.justify-content-around.align-items-center
+          .col-3.col-sm-2.col-lg-1
+            img(src="@/assets/curso/Tema1/pdf.png")
+          .col
+            .row.justify-content-between.align-items-center
+              .col.mb-3.mb-sm-0
+                h4.mb-2 Norma Técnica Sectorial Colombiana NTSH 002 de 2003
+                p.mb-0 Lo invitamos a visitar el siguiente PDF, donde podrá conocer los requisitos para atender sugerencias y reclamaciones de acuerdo con las políticas de servicio. 
+              .col-sm-auto
+                a.boton.color-acento-botones(href="https://www.mincit.gov.co/getattachment/minturismo/calidad-y-desarrollo-sostenible/calidad-turistica/normas-tecnicas-sectoriales/nts-alojamiento-y-hospedaje-1/norma-tecnica-sectorial-colombiana-ntsh-002-establ/norma-tecnica-sectorial-colombiana-ntsh-002.pdf.aspx" target="_blank")
+                  span Descargar 
+                  i.fas.fa-file-download
+
+    .tarjeta.color-acento-contenido.p-4.mb-4(data-aos="fade-right")
+        .row.justify-content-around.align-items-center
+          .col-3.col-sm-2.col-lg-1
+            img(src="@/assets/curso/Tema1/pdf.png")
+          .col
+            .row.justify-content-between.align-items-center
+              .col.mb-3.mb-sm-0
+                h4.mb-2 Protocolos de atención y servicio al cliente
+                p.mb-0 Lo invitamos a consultar el siguiente video, donde podrá conocer más a fondo sobre protocolos de atención y servicio al cliente.
+              .col-sm-auto
+                a.boton.color-acento-botones(href="https://www.youtube.com/watch?v=9o5sUuHmB-k" target="_blank")
+                  span Descargar 
+                  i.fas.fa-file-download
+    separador
+    #t_4_3.titulo-segundo.mb-5
+      h2 4.3 Ética del servicio 
+    
+    .row.justify-content-center.align-items-center
+      .col-lg-12
+        .bg1.mb-5
+          .row.justify-content-center.align-items-center.mb-2
+            .col-lg-5.d-none.d-lg-block(data-aos="fade-right")
+              .bg2
+                img.img-a.img-t(src='@/assets/curso/Tema4/img08.png', alt='')
+            .col-lg-7.col-10.p-1(data-aos="fade-left")
+              p(data-aos="fade-right") La ética en el servicio dentro de un área de reservas hoteleras se refiere al conjunto de principios y valores morales que guían el comportamiento de los profesionales encargados de gestionar las reservas de un hotel.  Implica actuar con integridad, honestidad, responsabilidad y respeto hacia los clientes, colegas y la empresa. 
+              p La ética es fundamental para construir y mantener la confianza de los clientes, mejorar la reputación del hotel y garantizar la satisfacción del cliente.
+        p Los siguientes son algunos aspectos clave de la ética en el servicio de reservas hoteleras:
+    
+    .row.justify-content-center.align-items-center.mb-5.fondotema43
+      .col-lg-9
+         PasosA.color-acento-contenido.mb-5(tipo="l")
+          .row.align-items-center
+            .col-md-8.mb-4.mb-md-0
+              h4 Honestidad y transparencia
+              p Proporcionar información precisa y completa sobre la disponibilidad, tarifas, políticas y servicios del hotel.
+              ul.lista-ul--color
+                li
+                  i.fas.fa-circle-check
+                  |Evitar prácticas engañosas o discriminatorias.
+                li
+                  i.fas.fa-circle-check
+                  |Ser transparente en el manejo de datos personales de los clientes.
+
+            .col-md-4
+              figure
+                img(src='@/assets/curso/Tema4/img09.png', alt='')
 
 
+          .row.align-items-center.justify-content-start
+            .col-md-4
+              figure
+                img(src='@/assets/curso/Tema4/img10.png', alt='')
+            .col-md-8.mb-4.mb-md-0
+              h4 Confidencialidad
+              p Proteger la información personal y financiera de los clientes.
+              ul.lista-ul--color
+                li
+                  i.fas.fa-circle-check
+                  |No divulgar información confidencial a terceros no autorizados.
+
+          .row
+            .col-md-8.mb-4.mb-md-0
+              h4 Respeto y empatía
+              p Tratar a todos los clientes con cortesía, amabilidad y respeto, independientemente de su origen, raza, religión o condición social.
+              ul.lista-ul--color
+                li.d-flex.mb-0
+                  i.fas.fa-circle-check
+                  p Mostrar empatía y comprensión ante las necesidades y preocupaciones de los clientes.
+
+                li
+                  i.fas.fa-circle-check
+                  |La inteligencia emocional es fundamental.
+            .col-md-4
+              figure
+                img(src='@/assets/curso/Tema4/img11.png', alt='')
+
+          .row.align-items-center.justify-content-start
+            .col-md-4
+              figure
+                img(src='@/assets/curso/Tema4/img12.png', alt='')
+            .col-md-8.mb-4.mb-md-0
+              h4 Responsabilidad
+              p Cumplir con los compromisos adquiridos con los clientes.
+              ul.lista-ul--color
+                li
+                  i.fas.fa-circle-check
+                  |Resolver problemas y quejas de manera oportuna y eficiente.
+                li.d-flex
+                  i.fas.fa-circle-check
+                  p Asumir la responsabilidad de los errores y tomar medidas correctivas.
+
+          .row.align-items-center
+            .col-md-8.mb-4.mb-md-0
+              h4 Profesionalismo
+              p Mantener una actitud profesional en todo momento.
+
+              ul.lista-ul--color
+                li
+                  i.fas.fa-circle-check
+                  |Cumplir con las normas y procedimientos del hotel.
+
+                li
+                  i.fas.fa-circle-check
+                  |Buscar la mejora continua en la calidad del servicio.
 
 
-
-
-
-
-
-
-
-
-
-
-
+            .col-md-4
+              figure
+                img(src='@/assets/curso/Tema4/img13.png', alt='')
 
 
 
